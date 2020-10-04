386399abe7f6f6f83bb77f8ea6bc1197e343d40c
Byron Dominguez <bdominguez@MacBook-Pro-de-Timmy.local>
2020-10-04 13:07:03 -0500

    test(backend): :sparkles: add farm create test

--- Makefile
+++ Makefile
@@ -14,3 +14,6 @@ ping-mongo:
 
 build:
 	@docker-compose build
+
+test:
+	@docker-compose exec -T backend npm run test
---
+++ backend/__tests__/src/modules/farms/shared/domain/bill_test.ts
@@ -0,0 +1,23 @@
+import { Farm } from '../../../../../../src/modules/farms/domain/Farm'
+const faker = require('faker')
+
+faker.locale = 'es'
+
+let farm: Farm
+let data: any = {
+  name: faker.name.findName(),
+  farm_size: 0,
+}
+
+beforeEach(() => {
+  farm = Farm.create(data.name)
+})
+
+describe('Domain Farm', () => {
+  it('should be able to create a farm', async () => {
+    expect(farm.name).toEqual(data.name)
+  })
+  it('should create a farm with size 0', async () => {
+    expect(farm.farm_size).toEqual(data.farm_size)
+  })
+})
